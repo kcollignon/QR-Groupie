858afd0595f2bda134e54f8906b5b3577aefae5d
Kevin Collignon <kevin@kevincollignon.com>
2012-01-27 20:53:54 -0700

    implement mobile device detection logic

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,9 +1,11 @@
 class Track < ActiveRecord::Base
-  has_attached_file :song,
+
+belongs_to :album
+
+has_attached_file :song,
        :storage => :s3,
        :s3_credentials => "#{Rails.root}/config/s3.yml",
        :path => "/:style/:id/:filename",
        :bucket => "qr_songs"
-  
-  belongs_to :album
+
 end
