3b03e0f1de68c50d39fe153ad724eaf4fe1f7849
Kevin Collignon <kevin@kevincollignon.com>
2012-02-11 21:00:20 -0700

    Add username to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120211100519) do
+ActiveRecord::Schema.define(:version => 20120212035058) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(:version => 20120211100519) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
