83e4e9a63c5dc77c42b74d2cffc0aac698d140a0
Kevin Collignon <kevin@kevincollignon.com>
2012-02-11 21:32:20 -0700

    User can login with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :username, :login
+
+  attr_accessor :login
 
   after_create :send_welcome_email
 
   def send_welcome_email
   	UserMailer.welcome_email(self.email).deliver
   end
+
+  def self.find_for_database_authentication(warden_conditions)
+   conditions = warden_conditions.dup
+   login = conditions.delete(:login)
+   where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
+ end
 
 end
